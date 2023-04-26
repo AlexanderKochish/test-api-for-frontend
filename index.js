842c009ea34795e2a92ea06f28069efedabbc370
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-26 22:27:31 +0300

    good

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.use(express.static('public'));
 app.use(multer({ storage: storageConfig }).single("photo"));
+app.use(express.static('public'));
 
 const swaggerDocument = YAML.load('./swagger.yaml');
 
