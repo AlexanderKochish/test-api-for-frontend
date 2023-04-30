010ce8c77a450f211a82ac33f0cf02708946187a
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-30 19:29:14 +0300

    crone 6

--- clearDB.js
+++ clearDB.js
@@ -4,9 +4,7 @@ const sequelize = require('./db')
 
 const deleteOldUsers = async() => {
     try {
-        const users = await User.findAll({where:{id:{[Op.gt]: 10}}});
         await User.destroy({where:{id:{[Op.gt]: 10}}})
-        return users;
     } catch (error) {
         console.log(error.message);
     }
