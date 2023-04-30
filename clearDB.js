c5b0f1766ae25cb3e091be0251f3881fa4e71834
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-04-30 19:23:21 +0300

    cron 5

--- clearDB.js
+++ clearDB.js
@@ -1,19 +1,12 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize, Op } = require('sequelize')
 const {User} = require('./models/models')
 const sequelize = require('./db')
 
 const deleteOldUsers = async() => {
     try {
-        const users = await User.findAll({
-            order:[['createdAt', 'DESC']],
-            offset: 10,
-        });
-
-        await User.destroy({
-            where:{
-                id: users.map(user => user.id)     
-            }
-        })
+        const users = await User.findAll({where:{id:{[Op.gt]: 10}}});
+        await User.destroy({where:{id:{[Op.gt]: 10}}})
+        return users;
     } catch (error) {
         console.log(error.message);
     }
--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.set("x-powered-by","")
 app.use(notFound)
 app.use(errorHandler)
-cron.schedule('10 19 * * *',() => deleteOldUsers)
+cron.schedule('25 19 * * *',() =>{ deleteOldUsers()})
 
 async function start() {
   try {
