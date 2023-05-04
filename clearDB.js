32cdeee5b593ddc849b11c18da2ae7fde493104b
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-05-05 02:21:42 +0300

    refactoring sort

--- clearDB.js
+++ clearDB.js
@@ -1,12 +1,10 @@
-const { Sequelize, Op, where } = require('sequelize')
+const { Op } = require('sequelize')
 const {User} = require('./models/models')
-const sequelize = require('./db')
 
 const deleteOldUsers = async() => {
     try {
         const allUsers = await User.findAll({limit: 10})
         await User.destroy({where:{id:{[Op.gt]: 10}}})
-        console.log(`delete users`);
         return allUsers
     } catch (error) {
         console.log(error.message);
